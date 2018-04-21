b652fd8a44edbef3353120edc65c3080c177c896
huaixiaohao <15535436241@163.com>
2018-04-21 16:22:16 +0800

    0421

--- c++/point.cpp
+++ c++/point.cpp
@@ -1,57 +1,116 @@
 #include <iostream>
-#include <stdlib.h>
-#define PI 3.14
+#include <cstdlib>
 using namespace std;
+#define PI 3.1415926
 
+/**************  Point  ***************/
 class Point
 {
 public:
-	Point(int x, int y);
-	int Getrea();
+	Point(const int x = 0, const int y = 0);
+	void print();
+	void setX(int x);
+	void setY(int y);
+	virtual double area() = 0;
+	virtual double vol() = 0;
 
-private:
-	int m_x;
-	int m_y;
+protected:
+	int p_x;
+	int p_y;
+
 };
+
+Point::Point(const int x, const int y)
+{
+	p_x = x;
+	p_y = y;
+}
+
+void Point::setX(int x)
+{
+	p_x = x;
+}
+
+void Point::setY(int y)
+{
+	p_y = y;
+}
+
+void Point::print()
+{
+	cout << "x = " << p_x << "y = " << p_y << endl;
+}
+
+/**************  Circle  ***************/
+
 class Circle : public Point
 {
 public:
-	Circle(int r);           //构造函数，并用r构造基类的x和y。  
-	double GetArea();           //求圆的面积。  
-	double GetRadius();         //获取圆的半径。  
+	Circle(double r = 0.0, int x = 0, int y = 0);
+	double area();
+	void c_print();
+
+protected:
+	double c_r;
+
 };
 
-class Rectangle:public Point
+Circle::Circle(double r, int x, int y) : Point(x, y)
+{
+	c_r = r;
+}
+
+double Circle::area()
+{
+	cout << c_r << endl;
+	return PI * c_r * c_r;
+}
+
+void Circle::c_print()
+{
+	cout << "circle's area is " << area() << endl;
+}
+
+/**************  Cylinder  ***************/
+class Cylinder : public Circle
 {
 public:
-	Rectangle(double l, double w);      //构造函数，并用l和w构造基类的x和y。  
-	double GetArea();                   //求矩形的面积。  
-	double GetLength();                 //获取矩形的长。  
-	double GetWidth();                  //获取矩形的宽。      
+	Cylinder(double h, double r, int x, int y);
+	double vol();
+	void cy_setH(double h);
+	void cy_print();
+
+private:
+	double cy_h;
+
 };
 
-Point::Point(int x, int y)
+Cylinder::Cylinder(double h, double r, int x, int y):Circle(r, x, y)
 {
-	m_x = x;
-	m_y = y;
-}
-int Point::Getrea()
-{
-	return 0;
+	cy_h = h;
 }
 
-Circle::Circle(int r) :Point(r, r) {}
-double Circle::GetArea()
+double Cylinder::vol()
 {
-	return PI*r*r;
+	cout << cy_h << endl;
+	return area() * cy_h;
 }
-double Circle::GetRadius()
+
+void Cylinder::cy_setH(double h)
 {
-	return r;
+	cy_h = h;
+}
+
+void Cylinder::cy_print()
+{
+	cout << "cylinder's vol is " << vol() << endl;
 }
 
 int main()
 {
-	system("psuse");
+	Cylinder cylin(5.0, 2.0, 1, 1);
+	cylin.c_print();
+	cylin.cy_print();
+	system("pause");
 	return 0;
 }
